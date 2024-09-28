d8f7e459b2c36d17e1378c46a5c049f404814b49
JmBears <lovelateacup@gmail.com>
2024-09-28 11:12:09 +0800

    [ASSIGNMENT] Behavior Tree Implementation
    I am miserable. I can listen and read Chinese, I cannot write or speak it.

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -9,18 +9,35 @@ public class Inventory : MonoBehaviour
 
     private void Awake()
     {
-        ActionNode actionNode = new(CheckInventoryForMeat);
+        ActionNode actionNode = new(CheckInventoryForFood);
     }
     public bool ContainsItem(string id)
     {
         return (items.Contains(id));
     }
 
-    public NodeState CheckInventoryForMeat()
+    //  Wouldn't it be better if it's just CheckInventoryForFood and have an if(items.Contains("Meat")||if(items.Contains("Vegetable")||if(items.Contains("Fruit"))?
+    public NodeState CheckInventoryForFood()
     {
-        if (items.Contains("Meat"))
+        if (items.Contains("Meat")||(items.Contains("Vegetable")||(items.Contains("Fruit"))))
             return NodeState.SUCCESS;
         else
             return NodeState.FAILURE;
     }
+
+    /*public NodeState CheckInventoryForVegetable()
+    {
+        if (items.Contains("Vegetable"))
+            return NodeState.SUCCESS;
+        else
+            return NodeState.FAILURE;
+    }
+
+    public NodeState CheckInventoryForFruit()
+    {
+        if (items.Contains("Fruit"))
+            return NodeState.SUCCESS;
+        else
+            return NodeState.FAILURE;
+    }*/
 }
